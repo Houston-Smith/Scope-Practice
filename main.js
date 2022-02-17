5cb0518d545473184b168bf980fa383435584827
Houston-Smith <houstonbsmith@gmail.com>
2022-02-17 14:07:59 -0600

    Initial Commit

---
+++ main.js
@@ -0,0 +1,88 @@
+
+// C IS FOR COOKIE
+
+// const cookies = ["Oatmeal Raisin", "Chocolate Chip", "Sugar", "Peanut Butter", "Snickerdoodle", "Ginger"]
+
+
+// for (let x = 1; x < cookies.length; x++) {
+//     const currentCookie = cookies[x]
+//     console.log(`Mmmmmmm... that's a good ${currentCookie} cookie`)
+// }
+
+
+
+
+// CONJUNCTION FUNTION
+
+// const conjunction = function (firstWord, secondWord) {
+//   const conjoinedWord = `${firstWord} ${secondWord}`
+//   return conjoinedWord
+// }
+
+// conjoinedWord = conjunction("Master", "Card")
+// console.log(conjoinedWord)
+
+
+
+
+// MOD SQUAD
+
+// const modSquad = {
+//       "members": ["Pete Cochran", "Linc Hayes", "Julie Barnes", "Capt. Adam Greer", "Chief Barney Metcalf"],
+//       "series": {
+//           "start": "1968",
+//           "end": "1973"
+//       }
+//   }
+
+// let HTMLRepresentation = `<h1>The Mod Squad</h1>`
+
+// modSquad.members.forEach(member => {
+//       HTMLRepresentation += `<div>${member}</div>`
+//   })
+
+
+// console.log(HTMLRepresentation)
+
+
+
+// SIMON SAYS ??
+
+// const locations = [[1,1], [1,2], [1,3], [2,1], [2,2], [2,3]]
+
+// for (const location of locations) {
+
+//     if (location[0] > 2) {
+//         const invalidLocation = true
+
+//           if (invalidLocation) {
+//               console.log("This location is invalid")
+//           }
+//     }
+    
+//     else {
+//       console.log("This location is valid")
+//     }
+//   }
+
+
+
+// LAMBDA LLAMA
+
+// const llamaNamer = function () {
+//     const possibleNames = ["Larry", "Leon", "Leona", "Les", "Laura", "Lemony", "Lars", "Lekisha"]
+//     const randomizer = Math.floor(Math.random() * 7)
+
+//     const namer = function () {
+//         const suffix = " the Llama"
+//         const name = possibleNames[randomizer]
+//         return name + suffix
+//     }
+
+//     llama = namer()
+//     return llama
+
+//   }
+
+// nameMaker = llamaNamer()
+// console.log(nameMaker)
